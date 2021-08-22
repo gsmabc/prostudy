167d85f0c1f784348c9f0c7a97eae588c785066d
gsmabc <gsm7779@163.com>
2021-08-22 18:42:20 +0800

    辗转相除法2021-8-22-1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
-#include <iostream>
-//辗转相除法
+#include<iostream>
+#include<string>
+
 using namespace std;
 
-int gcd(int a, int b) {
-	if (b == 0) {
-		return a;
-	} else {
-		return gcd(b, a % b);
+string MtoN(int m, int n) {
+	char table[16];       //依次表示01234...9ABCDEF（表示0~15）
+	if (m < 0)               //负数转换为正数
+		m = m * (-1);
+	for (int i = 0; i < 16; i++) {
+		if (i < 10)
+			table[i] = i + '0';       //转换为字符是是（加）+'0',不能为（减）-'0'.
+		else
+			table[i] = i - 10 + 'A';
 	}
+	string res;
+	while (m) {
+		int temp = m % n;
+		char t = table[temp];
+		res = t + res;
+		m = m / n;
+	}
+	return res;
 }
 
 int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << a * b / gcd(a, b);
-	return 0;
+	int m, n;
+	cin >> m >> n;
+
+	string res = MtoN(m, n);
+	if (m < 0)
+		cout << "-";
+	cout << res << endl;
+
 }
