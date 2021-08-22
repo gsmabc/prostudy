46b06da1937f05b94b920d537dfd1dd98fe3e7e0
gsmabc <gsm7779@163.com>
2021-08-22 20:19:41 +0800

    回形(顺)矩阵 2021-8-22-4

--- main.cpp
+++ main.cpp
@@ -1,29 +1,33 @@
-//反之矩阵
-#include <iostream>
+#include<iostream>;
 using namespace std;
-int a[11][11];
+
 int main() {
-	int N, count = 1;
-	cin >> N;
-	for (int i = N - 1; i >= 0; i--) {
-		if (i % 2 == 1) {
-			for (int j = N - 1; j >= 0; j--) {
-				a[i][j] = count++;
-
-			}
-		} else {
-			for (int j = 0; j < N; ++j) {
-				a[i][j] = count++;
-
-			}
+	int n;
+	cin >> n;
+	int a[99][99];
+	int count = 1;
+	for (int i = 0; i < n / 2 + 1; i++) {
+		// up
+		for (int j = i; j < n - i; j++) {
+			a[i][j] = count++;
+		}
+		// right
+		for (int j = i + 1; j < n - i; j++) {
+			a[j][n - i - 1] = count++;
+		}
+		// down
+		for (int j = n - i - 2; j >= i; j--) {
+			a[n - i - 1][j] = count++;
+		}
+		// left
+		for (int j = n - i - 2; j > i; j--) {
+			a[j][i] = count++;
 		}
 	}
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - 1; ++j)
 			cout << a[i][j] << " ";
-
-		}
-		cout << endl;
+		cout << a[i][n - 1] << endl;
 	}
 	return 0;
 }
