30755c40a682e384b0d56aa942d8e4025023ba12
gsmabc <gsm7779@163.com>
2021-08-29 18:33:31 +0800

    小写转大写 2021-8-29-2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 #include <cstring>
-//字符替换
+//小写转大写
 
 using namespace std;
 
 int main() {
 	char s[201];
-	char A, B;
+
 	gets(s);
-	cin >> A >> B;
 	int n = strlen(s);
 	for (int i = 0; i < n; ++i) {
-		if (s[i] = A)
-			cout << B;
+		if (s[i] >= 90)
+			cout << char(s[i] - 32);
 		else
 			cout << s[i];
 
