feae3995b4b1bfc4736cbb414ab554b453522054
gsmabc <gsm7779@163.com>
2021-09-20 19:34:01 +0800

    统计有效字母/数字 2021-9-20-1

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
+//(rand() % (b-a+1))+ a;
+#include<iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
-//去空格/指定字符
-string str_stp(string str, char c = ' ') {
-	string s;
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] != c) {
-			s += str[i];
+//统计有效字母/数字
+int main() {
+	char a[100];
+	int count = 0;
+	gets(a);
+	for (int i = 0; i < strlen(a); ++i) {
+		if (isalpha(a[i]) || isdigit(a[i])) {
+			count++;
 		}
 	}
-	return s;
-}
+	cout << count;
 
-int main() {
-	string str;
-	getline(cin, str);
-	char c;
-	cin >> c;
-	if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z') {
-		cout << str_stp(str, c);
-	} else {
-		cout << str_stp(str);
-	}
+
 	return 0;
 }
