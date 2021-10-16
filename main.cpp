3b0a1df5dd2ca7b9373b6bd97b5bdb5518334ea3
gsmabc <gsm7779@163.com>
2021-10-16 20:54:12 +0800

    辗转相除法-最大公约数 2021-10-16-1

--- main.cpp
+++ main.cpp
@@ -1,34 +1,43 @@
-//(rand() % (b-a+1))+ a;
+#include <iostream>
+//辗转相除法-最大公约数
 #include<iostream>
-#include <cmath>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
-#include <cctype>
-#include <cstring>
-#include <algorithm>
+#include<string>
 
 using namespace std;
 
-bool camp(double a, double b) {
-	return a > b;
+string MtoN(int m, int n) {
+	char table[16];       //依次表示01234...9ABCDEF（表示0~15）
+	if (m < 0)               //负数转换为正数
+		m = m * (-1);
+	for (int i = 0; i < 16; i++) {
+		if (i < 10)
+			table[i] = i + '0';       //转换为字符是是（加）+'0',不能为（减）-'0'.
+		else
+			table[i] = i - 10 + 'A';
+	}
+	string res;
+	while (m) {
+		int temp = m % n;
+		char t = table[temp];
+		res = t + res;
+		m = m / n;
+	}
+	return res;
 }
 
-//性价比
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	} else {
+		return gcd(b, a % b);
+	}
+}
+
 int main() {
-	double b[100];
-	double n, x, y;
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		cin >> x >> y;
-		b[i] = x * 1.0 / y;
-	}
-	sort(b, b + int(n), camp);
-	for (int i = 0; i < n; ++i) {
-		cout << fixed << setprecision(3) << b[i] << " ";
-	}
-
-
+	int a, b;
+	cin >> a >> b;
+	cout << a * b / gcd(a, b);
 	return 0;
-}
 
+
+}
