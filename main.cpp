73d5db805bad706f3211a6901d0e1d9ce075c1f4
gsmabc <gsm7779@163.com>
2021-10-30 21:01:21 +0800

    20211030-4 冒泡排序小到大

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
-//20211030-2
-//数塔
+//20211030-4
+//冒泡排序小到大
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 
 using namespace std;
-
 
 int main() {
 	int n;
 	cin >> n;
-	int a[n + 1][n + 1];
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= i; ++j) {
-			cin >> a[i][j];
-		}
+	int a[n + 1];
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
 	}
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 1; j <= i; j++) {
-			if (a[i + 1][j] >= a[i + 1][j + 1]) {
-				a[i][j] += a[i + 1][j];
-			} else {
-				a[i][j] += a[i + 1][j + 1];
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
 			}
-
 		}
-
 	}
-	cout << a[1][1];
+	for (int i = 1; i <= n; i++) {
+		cout << a[i] << " ";
+	}
 	return 0;
 }
+
