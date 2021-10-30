f08a9798f616517311e5d603a1b519c276aa33dc
gsmabc <gsm7779@163.com>
2021-10-30 21:02:47 +0800

    20211030-5 冒泡排序大到小

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//20211030-4
-//冒泡排序小到大
+//20211030-5
+//冒泡排序大到小
 #include <iostream>
 
 using namespace std;
@@ -13,7 +13,7 @@ int main() {
 	}
 	for (int i = 1; i <= n - 1; i++) {
 		for (int j = 1; j <= n - i; j++) {
-			if (a[j] > a[j + 1]) {
+			if (a[j] < a[j + 1]) {
 				swap(a[j], a[j + 1]);
 			}
 		}
