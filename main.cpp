4ce818c41fe3211b0cd4d0dcae5928611d5dae46
gsmabc <gsm7779@163.com>
2021-12-25 18:49:31 +0800

    114514

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 using namespace std;
-
 int main() {
-	int f[501], n;
-	f[1] = 1, f[2] = 1;
-	cin >> n;
-	if (n < 3) cout << 1;
-	else {
-		for (int i = 3; i <= n; i++)
-			f[i] = 3 * f[i - 1] + 2 * f[i - 2];
-		cout << f[n];
+	int num, i, sum = 0;
+	for (i = 0; i < 25; i++) {
+		cin >> num;
+		sum += num;
 	}
+	sum = 182 - sum;
+	cout << sum;
 	return 0;
 }
