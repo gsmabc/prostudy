31608a2c36d2f1210231a0eefeb656cd8f7110b0
gsmabc <gsm7779@163.com>
2022-01-08 20:12:04 +0800

    no

--- main.cpp
+++ main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 
 using namespace std;
+int t[3] = {1, 2, 3};
+
+void setDice(const char d) {
+	int u, f, r;
+	switch (d) {
+		case 'R':
+			u = 7 - t[2];
+			f = t[1];
+			r = t[0];
+			break;
+		case 'D':
+			u = 7 - t[1];
+			f = t[0];
+			r = t[2];
+			break;
+		case 'L':
+			u = t[2];
+			f = t[1];
+			r = 7 - t[0];
+			break;
+	}
+	t[0] = u;
+	t[1] = f;
+	t[2] = r;
+}
 
 int main() {
-	int n, m, p[10000] = {}, b[10000] = {};
-	cin >> n >> m;
-	for (int k = 1; k <= 2 * n; k++)
-		p[k] = k;
-	for (int i = 1; i <= m; i++) {
-		for (int k = 1; k <= n; k++) {
-			b[2 * k] = p[k];
-			b[2 * k - 1] = p[n + k];
+	int r, c, sum = 0;
+	bool flag = true;
+	cin >> r >> c;
+	for (int i = 1; i <= r; i++) {
+		if (flag) {
+			for (int j = 1; j < c; j++) {
+				sum += t[0];
+				setDice('R');
+			}
+		} else {
+			for (int j = c; j > 1; j--) {
+				sum += t[0];
+				setDice('L');
+			}
 		}
-		for (int k = 1; k <= 2 * n; k++)
-			p[k] = b[k];
+		sum += t[0];
+		setDice('D');
+		flag = !flag;
 	}
-	for (int k = 1; k <= 2 * n; k++) {
-		cout << p[k] << ' ';
-	}
+	cout << sum;
 	return 0;
 }
