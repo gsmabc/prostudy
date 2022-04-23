b97aa25c403297092f5be3b73c17149261eb8172
gsmabc <gsm7779@163.com>
2022-04-23 18:28:44 +0800

    no

--- WallPaper2.py
+++
@@ -1,9 +0,0 @@
-with open('fff.txt', 'r', encoding='utf-8') as f:
-    s = f.read()
-    f.close()
-l = set(s.split('\n'))
-with open('fff1.txt', 'w+', encoding='utf-8') as f:
-    for i in l:
-        f.write(i + '\n')
-    f.close()
-print('done')
--- main.cpp
+++ main.cpp
@@ -1,32 +1,6 @@
-﻿#include<iostream>
-#include<algorithm>
-
+#include<iostream>
 using namespace std;
-struct things {
-	string name;
-	double weight;
-};
-
-bool cmp(things a, things b) {
-	return a.weight < b.weight;
-}
-
 int main() {
-	things w1[101];
-	int c, n;
-	cin >> c >> n;
-	for (int i = 0; i < n; i++)
-		cin >> w1[i].name >> w1[i].weight;
-	sort(w1, w1 + n, cmp);
-	int sum = 0, count = 0;
-	for (int i = 0; i < n; i++) {
-		sum += w1[i].weight;
-		if (sum <= c) {
-			count++;
-		} else
-			break;
-	}
-	for (int i = 0; i < count; i++)
-		cout << w1[i].name << " ";
+
 	return 0;
 }
