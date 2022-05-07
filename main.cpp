f0f6c7c4559e6eb5feb99fda892663ab655ac17d
gsmabc <gsm7779@163.com>
2022-05-07 19:54:22 +0800

    无向图深搜

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include<iostream>
-
 using namespace std;
 int g[101][101];
-
-int main() {
-	int a, b, n, m;
-	cin >> n >> m;
-	for (int k = 1; k <= m; k++) {
-		cin >> a >> b;
-		g[a][b] = 1;
-		g[b][a] = 1;
+int visited[101];
+int n,e;
+int a,b;
+int sum=0;
+void dfs(int cur) {
+	cout << cur <<" ";
+	sum++;
+	if(sum==n)
+		return;
+	for(int i=1; i<=n; i++) {
+		if(g[cur][i]==1&&visited[i]!=1) {
+			visited[i]=1;
+			dfs(i);
+		}}}
+int main(){
+	cin>>n>>e;
+	for(int i=1; i<=e; i++) {
+		cin>>a>>b;
+		g[a][b]=1;
+		g[b][a]=1;
 	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << g[i][j];
-		}
-		cout << endl;
-	}
+	visited[1]=1;
+	dfs(1);
 	return 0;
 }
