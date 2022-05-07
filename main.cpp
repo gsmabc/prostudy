d803b502d9b45f44207bb910340a84d877757960
gsmabc <gsm7779@163.com>
2022-05-07 19:37:09 +0800

    构建无权无向图

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
-using namespace std;
-int main() {
 
+using namespace std;
+int g[101][101];
+
+int main() {
+	int a, b, n, m;
+	cin >> n >> m;
+	for (int k = 1; k <= m; k++) {
+		cin >> a >> b;
+		g[a][b] = 1;
+		g[b][a] = 1;
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << g[i][j];
+		}
+		cout << endl;
+	}
 	return 0;
 }
