f3f9afd15e1cdd8aeec0d643305fd70fa895c5f3
gsmabc <gsm7779@163.com>
2022-10-24 22:47:48 +0800

    https://www.luogu.com.cn/problem/P8109（re原因：数组开小了

--- main.cpp
+++ main.cpp
@@ -1,63 +1,24 @@
-#include <iostream>
-#include <cmath>
+#include <bits\stdc++.h>
 
 using namespace std;
-int a[100010][3];
 
 int main() {
-	int i = 1, j = 1, k, expect, down, max, temp, cha, xl, num, s, price, p;
-	cin >> expect;
-	while (cin >> a[i][1] >> a[i][2] && a[i][1] != -1 && a[i][2] != -1) {
-		i++;
-		if (i > 2 && a[i - 1][1] - a[i - 2][1] > 1) {
-			i--;
-			cha = (a[i - 1][2] - a[i][2]) / (a[i][1] - a[i - 1][1]);
-			temp = a[i][1];
-			for (j = a[i - 1][1] + 1; j <= temp; j++) {
-				a[i][1] = j;
-				a[i][2] = a[i - 1][2] - cha;
-				i++;
-			}
-		}
+	int n = 0, a[1000005] = {}, b[1000005] = {}, total = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
 	}
-	cin >> down;
-	i--;
-	xl = a[i][2];
-	while (xl > 0) {
-		if (xl - down < 0)break;
-		else {
-			xl -= down;
-			i++;
-			a[i][1] = a[i - 1][1] + 1;
-			a[i][2] = xl;
-		}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
 	}
-	for (j = 1; j <= 10000; j++) {
-		max = -99999;
-		for (k = 1; k <= i; k++) {
-			num = (a[k][1] - a[1][1] + j) * a[k][2];
-			if (num >= max) {
-				max = num;
-				price = a[k][1];
-				p = 1;
-			}
-		}
-		if (price == expect) {
-			cout << j * p;
-			return 0;
-		}
-		max = -99999;
-		for (k = 1; k <= i; k++) {
-			num = (a[k][1] - a[1][1] - j) * a[k][2];
-			if (num >= max) {
-				max = num;
-				price = a[k][1];
-				p = -1;
-			}
-		}
-		if (price == expect) {
-			cout << j * p;
-			return 0;
-		}
+	for (int i = 0; i < n; i++) {
+		total += (min(a[i], b[i]));
 	}
+	cout << total;
 }
+//using namespace std; int a[100005],b[100005],n,total;
+//int main(){
+//	cin>>n;
+//	for(int i=1;i<=n;i++){ cin>>a[i]; } for(int i=1;i<=n;i++)cin>>b[i];
+//	for(int i=1;i<=n;i++)total+=min(a[i],b[i]); cout<<total;
+//}
